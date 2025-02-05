ad1b8ea67923608fd4819d6279a173d0f73b4e2b
muzamil8888 <mmuzammilmeo456@gmail.com>
2025-02-05 21:28:20 +0500

    third

---
+++ computer-guess3/third.py
@@ -0,0 +1,18 @@
+import random
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ' '
+    while feedback != 'c': 
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low    
+        feedback = input(f"Is {guess} too hight (H), too low (L), or correct (C)??").lower()
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
+    print(f"YAY ! The computer guessed your number, {guess},Correctly!")        
+computer_guess(100)        
