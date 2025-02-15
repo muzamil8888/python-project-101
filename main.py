00128b9585d487fbe135c8c745dc9ce34b17e49e
muzamil8888 <mmuzammilmeo456@gmail.com>
2025-02-15 14:46:30 +0500

    chamged

---
+++ main.py
@@ -0,0 +1,10 @@
+adj = input("Adjective:for example Amazing,nice,excillent")
+verb1 = input("Name first programing language:")
+verb2 = input("Name of second programing language:")
+famperson = input("famousPerson name")
+
+
+print(f"""
+I have a {adj} computer.and i am learning {verb1} on it and  i also learning {verb2} and when i complete it i feel like a 
+{famperson}
+""")
